297b76b5c756b0a6d9099cefe668f350783587f7
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-09-01 00:08:37 -0300

    Corrigindo nome da classe

--- src/Banco.java
+++ src/Banco.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Banco {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nome = "Alex Ramos Ribeiro";
